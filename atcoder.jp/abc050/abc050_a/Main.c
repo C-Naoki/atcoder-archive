2d1c691118ffec1e800608747cf9ce2da7da7161
C_NaoKi <u429561d@ecs.osaka-u.ac.jp>
2020-06-20 14:24:55 +0000

    [AC] abc050 abc050_a

---
+++ atcoder.jp/abc050/abc050_a/Main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+  int a,b;
+  char s;
+  scanf("%d %c %d",&a,&s,&b);
+  if (s=='+'){
+    printf("%d\n",a+b);
+  }else{
+    printf("%d\n",a-b);
+  }
+  return 0;
+}
